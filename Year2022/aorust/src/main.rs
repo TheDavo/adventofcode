20ce038803088c0efb2de5868c57d53173f58be9
Davit Khudaverdyan <davkhuda@gmail.com>
2022-12-04 10:50:05 +0000

    Implement day 3 solution

--- Year2022/aorust/src/main.rs
+++ Year2022/aorust/src/main.rs
@@ -4,4 +4,6 @@ fn main() {
     println!("Day 1 Pt 2: {}", solutions::day1::solve_part_2());
     println!("Day 2 Pt 1: {}", solutions::day2::solve_part_1());
     println!("Day 2 Pt 2: {}", solutions::day2::solve_part_2());
+    println!("Day 3 Pt 1: {}", solutions::day3::solve_part_1());
+    println!("Day 3 Pt 2: {}", solutions::day3::solve_part_2());
 }
---
+++ Year2022/aorust/src/solutions/day3.rs
@@ -0,0 +1,43 @@
+use std::collections::HashSet;
+use itertools::Itertools;
+
+pub fn solve_part_1() -> usize {
+    let input = aorust::input_as_str(String::from("src/inputs/input3.txt"));
+
+    input.lines().map(|rucksack| {
+        let (left, right) = rucksack.split_at(rucksack.len() / 2);
+        let hleft: HashSet<char> = HashSet::from_iter(left.chars());
+        let hright: HashSet<char> = HashSet::from_iter(right.chars());
+        let misplaced_item: Option<&char> = hleft.intersection(&hright).nth(0); // Option
+        if let Some(item) = misplaced_item {
+            get_item_priority(*item) 
+        } else {
+            0
+        }
+    }).sum()
+}
+
+pub fn solve_part_2() -> usize {
+    let input = aorust::input_as_str(String::from("src/inputs/input3.txt"));    
+
+    input.lines().tuples::<(_,_,_)>().map(|(first, second, third)| -> usize {
+        let hfirst: HashSet<char> = HashSet::from_iter(first.chars());
+        let hsecond: HashSet<char> = HashSet::from_iter(second.chars());
+
+        let possible_badges = hfirst.intersection(&hsecond);
+        possible_badges.map(|poss_badge| -> usize {
+            if third.contains(*poss_badge) {
+                get_item_priority(*poss_badge)
+            } else {
+                0
+            }
+        }).sum::<usize>()
+    }).sum::<usize>()
+}
+
+fn get_item_priority(item: char) -> usize {
+    match item.is_uppercase() {
+        true => item as usize - 'A' as usize + 27,
+        false => item as usize - 'a' as usize + 1
+    }
+}
