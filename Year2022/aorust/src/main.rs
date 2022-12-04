1f6fea292aec31c4b29cf325a57a57b121f89c3e
Davit Khudaverdyan <davkhuda@gmail.com>
2022-12-04 21:24:22 +0000

    Solve day 4!

--- Year2022/aorust/src/main.rs
+++ Year2022/aorust/src/main.rs
@@ -6,4 +6,6 @@ fn main() {
     println!("Day 2 Pt 2: {}", solutions::day2::solve_part_2());
     println!("Day 3 Pt 1: {}", solutions::day3::solve_part_1());
     println!("Day 3 Pt 2: {}", solutions::day3::solve_part_2());
+    println!("Day 4 Pt 1: {}", solutions::day4::solve_part_1());
+    println!("Day 4 Pt 2: {}", solutions::day4::solve_part_2());
 }
