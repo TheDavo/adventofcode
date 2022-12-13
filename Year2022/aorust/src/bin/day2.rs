2872845fa7a27c41f59cad93567c2c463590143e
Davit Khudaverdyan <davkhuda@gmail.com>
2022-12-13 08:10:02 -0500

    Copy solutions into bin folder to prep for command line version of aorust.

---
+++ Year2022/aorust/src/bin/day2.rs
@@ -0,0 +1,133 @@
+enum GameResult {
+    Win,
+    Loss,
+    Draw,
+}
+
+impl GameResult {
+    fn get_game_result(elf_hand: &Hand, my_hand: &Hand) -> GameResult {
+        match elf_hand {
+            Hand::Rock => {
+                match my_hand {
+                    Hand::Rock => GameResult::Draw,
+                    Hand::Paper => GameResult::Win,
+                    Hand::Scissors => GameResult::Loss,
+                }
+            },
+            Hand::Paper => {
+                match my_hand {
+                    Hand::Rock => GameResult::Loss,
+                    Hand::Paper => GameResult::Draw,
+                    Hand::Scissors => GameResult::Win,
+                }
+            },
+            Hand::Scissors => {
+                match my_hand {
+                    Hand::Rock => GameResult::Win,
+                    Hand::Paper => GameResult::Loss,
+                    Hand::Scissors => GameResult::Draw,
+                }
+            },
+        }
+    }
+
+    fn get_game_value(&self) -> usize {
+        match self {
+            GameResult::Draw => 3,
+            GameResult::Loss => 0,
+            GameResult::Win => 6,
+        }
+    }
+
+    fn get_game_by_code(code : &str) -> GameResult {
+        match code {
+            "X" => GameResult::Loss,
+            "Y" => GameResult::Draw,
+            "Z" => GameResult::Win,
+            _ => panic!("Not a valid game type!"),
+        }
+    }
+}
+
+enum Hand {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl Hand {
+    fn new(hand : &str ) -> Hand{
+        match hand {
+            "A" | "X" => Hand::Rock,
+            "B" | "Y" => Hand::Paper,
+            "C" | "Z" => Hand::Scissors,
+            _ => panic!("Not an expected RPS hand"),
+
+        }
+    }
+
+    fn get_hand_value(&self) -> usize {
+        match self {
+            Hand::Rock => 1, 
+            Hand::Paper => 2,
+            Hand::Scissors => 3,
+        }
+    }
+
+    fn get_hand_for_game_result(elf: Hand, game_res: &GameResult) -> Hand {
+        match elf {
+            Hand::Rock => {
+                match game_res {
+                    GameResult::Loss => Hand::Scissors,
+                    GameResult::Draw => Hand::Rock,
+                    GameResult::Win => Hand::Paper,
+                }
+            }
+            Hand::Paper => {
+                match game_res {
+                    GameResult::Loss => Hand::Rock,
+                    GameResult::Draw => Hand::Paper,
+                    GameResult::Win => Hand::Scissors,
+                }
+            }
+            Hand::Scissors => {
+                match game_res {
+                    GameResult::Loss => Hand::Paper,
+                    GameResult::Draw => Hand::Scissors,
+                    GameResult::Win => Hand::Rock,
+                }
+            }
+        }
+    }
+}
+
+fn main () {
+    println!("Day 2 Pt 1: {}", solve_part_1());
+    println!("Day 2 Pt 2: {}", solve_part_2());
+}
+
+pub fn solve_part_1() -> usize {
+    let input = aorust::input_as_str(String::from("src/inputs/input2.txt"));
+
+    input.lines().map(|line| {
+        let (elf_code, my_code) = line.split_once(' ').unwrap();
+        let elf_hand: Hand = Hand::new(elf_code);
+        let my_hand: Hand = Hand::new(my_code);
+        
+        let game_result: GameResult = GameResult::get_game_result(&elf_hand, &my_hand); 
+        my_hand.get_hand_value() + game_result.get_game_value()
+    }).sum()
+    
+}
+
+pub fn solve_part_2() -> usize {
+    let input = aorust::input_as_str(String::from("src/inputs/input2.txt"));
+
+    input.lines().map(|line| {
+        let (elf_code, game_code) = line.split_once(' ').unwrap();
+        let elf_hand: Hand = Hand::new(elf_code);
+        let game: GameResult = GameResult::get_game_by_code(game_code);
+        let my_hand: Hand = Hand::get_hand_for_game_result(elf_hand, &game);
+        my_hand.get_hand_value() + game.get_game_value()
+    }).sum()    
+}
---
+++ Year2022/aorust/src/bin/day6.rs
@@ -0,0 +1,28 @@
+use itertools::Itertools;
+
+fn find_unique_marker(input: &str, marker_size: usize) -> usize {
+    let binding = input.chars().collect::<Vec<char>>();
+    let tupled = binding.windows(marker_size);
+
+    for (i, markers) in tupled.enumerate() {
+        if markers.iter().all_unique() {
+            return i + marker_size 
+        }
+    }
+    0
+}
+
+fn main () {
+    println!("Day 6 Pt 1: {}", solve_part_1());
+    println!("Day 6 Pt 2: {}", solve_part_2());
+}
+
+pub fn solve_part_1() -> usize {
+    let input = aorust::input_as_str(String::from("src/inputs/input6.txt"));
+    find_unique_marker(&input, 4)
+}
+
+pub fn solve_part_2() -> usize {
+    let input = aorust::input_as_str(String::from("src/inputs/input6.txt"));
+    find_unique_marker(&input, 14)
+}
